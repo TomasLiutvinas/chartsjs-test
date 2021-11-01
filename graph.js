93ec3493e78caad194937caa5d37fd871d7623d7
Tomas Liutvinas <tehdreamwalker@gmail.com>
2021-11-01 18:14:19 +0200

    First commit

---
+++ graph.js
@@ -0,0 +1,156 @@
+const dataString = "Grantas Kareckas,0,62,60,37,55,33,24;Zdislavas Giliunas,65,51,39,31,39,22,34;Darius Augulis,49,57,42,48,0,24,31;Aras Žemaitaitis,57,52,46,34,40,21,20;Deividas Nemura,38,48,51,33,38,12,34;Simonas Kazbaras,0,59,45,36,33,39,26;Tomas Jurkevičius,15,51,30,47,47,26,32;Laurynas Gončauskas,45,0,47,30,41,25,42;Karolis Smalskas,57,0,54,39,42,13,23;Šarūnas Skaburskis,42,34,39,20,30,24,29;Aidas Masaitis,62,24,29,32,35,0,12;Arvydas Macis,55,26,31,38,9,9,17;Tauras Norkūnas,54,36,23,12,24,0,11;Justinas Lapienis,26,0,35,23,38,18,14;Donatas Bieliauskas,34,27,22,14,23,6,4;Adas Paulius Paražinskas,2,40,27,17,12,15,9;Tomas Gradzevic,32,25,12,10,18,0,19;Tomas Liutvinas,17,39,20,8,8,4,18;Ignas Malinauskas,24,23,10,5,25,5,5;Egidijus Gervickas,9,11,4,7,16,0,27;Edgaras Alekna,18,9,5,9,20,0,3;Arūnas Gatelis,41,43,24,0,34,11,0;Marijus Kėbla,0,0,19,29,29,19,39;Aleksandras Rešetnikovas,36,54,18,0,19,0,0;Jonas Venčkauskas,53,55,17,0,0,0,0;Andrius Urbaitis,0,54,0,41,0,0,22;Justas Pirinauskas,0,0,25,28,36,0,28;Simonas Kvedaras,0,0,0,0,50,31,36;Žilvinas Lėckas,0,45,32,6,14,10,0;Andrius Lapienis,51,0,0,0,27,14,10;Aivaras Katinas,0,44,33,22,0,0,0;Mantas Sakalauskas,51,0,0,0,0,0,49;Aurimas Jazdauskas,20,41,37,0,0,0,0;Lukas Kybartas,46,37,11,0,0,0,0;Egidijus Miliauskas,0,65,0,27,0,0,0;Rūta Saladžienė,0,30,0,36,0,0,25;Donatas Rutkauskas,48,12,13,0,17,0,0;Gediminas Kasilauskas,40,49,0,0,0,0,0;Edvinas Katkevicius,0,46,43,0,0,0,0;Paulius Klemka,37,0,36,0,13,2,0;Audrius Mikučiūnas,44,0,41,0,0,0,0;Justinas Bumbulis,0,0,35,26,21,0,0;Kipras Valiulis,47,0,16,0,11,8,0;Mantas Kačerauskas,3,21,26,0,26,3,0;Mindaugas Kasilauskas,35,28,0,0,0,0,0;Petras Staupelis,30,33,0,0,0,0,0;Vytautas Zielys,0,35,28,0,0,0,0;Andrius Bareiša,0,42,0,19,0,0,0;Rojus Kunigėlis,59,0,0,0,0,0,0;Darius Janušas,0,0,0,18,22,17,0;Dominykas Graudinis,0,56,0,0,0,0,0;Simonas Kuodžius,33,22,0,0,0,0,0;Tomas Laucius,0,0,0,25,10,20,0;Sarunas Lengvinas,39,15,0,0,0,0,0;Modestas Tamulis,11,10,8,21,3,0,0;Mykolas Gudžius,52,0,0,0,0,0,0;Vytenis Sedleckas,27,16,9,0,0,0,0;Martynas Kučiauskas,0,14,6,0,31,0,0;Vytautas Marcinkevičius,0,5,15,16,7,1,7;Donatas Zalobaitis,0,38,0,0,0,0,13;Karolis Vilčinskas,28,0,0,0,0,0,21;Deividas Janušas,0,0,0,16,32,0,0;Marius Ragaisis,29,18,0,0,0,0,0;Orestas Burgaila,0,47,0,0,0,0,0;Jurij Lapo,43,0,0,0,0,0,0;Arnas Mikučiūnas,0,0,40,0,0,0,0;Laurynas Garuckas,0,0,0,24,15,0,0;Rytis Rumsas,31,7,0,0,0,0,0;Vytautas Davulis,22,0,14,0,0,0,0;Šarūnas Lengvinas,0,15,21,0,0,0,0;Tomas Mikolaitis,0,0,0,4,0,16,16;Domas Mickus,13,20,0,0,0,0,0;Martynas Vitkauskas,0,32,0,0,0,0,0;Ignas Vičkačka,0,0,0,0,0,0,30;Mindaugas Dirse,0,29,0,0,0,0,0;Rimantas Lakštauskas,25,0,0,0,0,0,0;Martynas Aladaitis,10,13,1,0,0,0,0;Justinas Buividas,23,0,0,0,0,0,0;Justinas Bisikirskas,0,21,0,0,0,0,0;Mykolas Chmieliauskas,21,0,0,0,0,0,0;Karolis Zalalis,0,19,0,0,0,0,0;Kipras Bunikis,19,0,0,0,0,0,0;Alvydas Šarutis,16,0,3,0,0,0,0;Mantas Burgaila,0,17,0,0,0,0,0;Pavel Fasij,0,0,0,0,0,0,15;Vytautas Dranseika,14,0,0,0,0,0,0;Laurynas Liužinas,0,0,0,13,0,0,0;Vytautas Vaičiukynas,12,0,0,0,0,0,0;Rokas Simutis,0,0,0,2,2,0,8;Rimantas Kišonas,0,4,0,0,5,0,0;Adomas Žarkaitis,8,0,0,0,0,0,0;Linas Matuzevičius,0,8,0,0,0,0,0;Julius Zvinys,7,0,0,1,0,0,0;Vidmantas Pauliukevičius,5,2,0,0,0,0,0;Julius Kybartas,0,0,7,0,0,0,0;Robertas Jauniškis,0,0,0,3,4,0,0;Eimantas Ruštas,0,0,0,0,0,7,0;Deividas Makarevičius,0,6,0,0,0,0,0;Simonas Valionis,6,0,0,0,0,0,0;Arūnas Kovalevskis,0,0,0,0,6,0,0;Paulius Kardelis,0,0,0,0,0,0,6;Mindaugas Murauskas,4,0,0,0,0,0,0;Ramūnas Kodis,0,3,0,0,0,0,0;Marius Baskys,0,0,2,0,0,0,0;Edvinas Baniulis,0,0,0,0,0,0,2;Emilija Toma Tamašauskaitė,1,0,0,0,0,0,0;Modestas Vansevicius,0,1,0,0,0,0,0;Darius Jankauskas,0,0,0,0,1,0,0;Lukas Liachovičius,0,0,0,0,0,0,1";
+let sourceDataArray = dataString.split(';');
+let raceDataArray = [];
+let championshipDataArray = [];
+
+const defaultPeople = [
+    "Tomas Liutvinas",
+    "Tomas Jurkevičius",
+    "Donatas Bieliauskas",
+    "Andrius Bareiša"
+];
+
+for (var i = 0; i < sourceDataArray.length; i++){
+    let label = sourceDataArray[i].split(',')[0];
+    let data = sourceDataArray[i].split(',').map(Number);
+    data.shift()
+
+    let champStandings = [];
+    let currentStanding = 0;
+
+    for(var j = 0; j < data.length; j++){
+        currentStanding += data[j];
+        champStandings.push(currentStanding);
+    }
+
+    let tension = 0.1;
+    let borderWidth = 2;
+    let hidden = true;
+    
+    if (defaultPeople.includes(label)){
+        // borderWidth = 1;
+        hidden = false;
+    }
+    
+
+    let r = getRandomInt(255);
+    let g = getRandomInt(255);
+    let b = getRandomInt(255);
+
+    raceDataArray.push(
+            { 
+            label: label,
+            data: data,
+            fill:false,
+            borderColor: `rgb(${r}, ${g}, ${b})`,
+            tension: tension,
+            borderWidth: borderWidth,
+            hidden:hidden
+            }
+        );
+
+    championshipDataArray.push(
+        {
+            label: label,
+            data: champStandings,
+            fill: false,
+            borderColor: `rgb(${r}, ${g}, ${b})`,
+            tension: tension,
+            borderWidth: borderWidth,
+            hidden: hidden
+        }
+    );
+}
+
+console.log(raceDataArray);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+$(function() {
+    const racesCtx = document.getElementById('races').getContext('2d');
+    const championshipCtx = document.getElementById('championship').getContext('2d');
+    
+    const labelsMine = [
+        "I",
+        "II",
+        "III",
+        "IV",
+        "V",
+        "VI",
+        "VII",
+    ];
+
+    const myDatasets = [{
+        label: 'Tomas Liutvinas',
+        data: [17,39,20,8,8,4,18],
+        fill: false,
+        borderColor: 'rgb(204, 0, 0)',
+        tension: 0.1
+    },
+    {
+        label: 'Donatas Bieliauskas',
+        data: [34,27,22,14,23,6,4],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    },
+    {
+        label: 'Tomas Jurkevičius',
+        data: [15,51,30,47,47,26,32],
+        fill: false,
+        borderColor: 'rgb(76, 0, 153)',
+        tension: 0.1
+    }
+];
+
+    const champDatasets = [{
+        label: 'Tomas Liutvinas',
+        data: [17, 17 + 39, 17 + 39 + 20, 17 + 39 + 20 + 8, 17 + 39 + 20 + 8 + 8, 17 + 39 + 20 + 8 + 8 + 4, 17 + 39 + 20 + 8 +8+4+18],
+        fill: false,
+        borderColor: 'rgb(204, 0, 0)',
+        tension: 0.1
+    },
+    {
+        label: 'Donatas Bieliauskas',
+        data: [34, 34 + 27, 34 + 27 + 22, 34 + 27 + 22 + 14, 34 + 27 + 22 + 14 + 23, 34 + 27 + 22 + 14 + 23 + 6, 34 + 27 + 22 + 14 + 23 +6+4],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    },
+    {
+        label: 'Tomas Jurkevičius',
+        data: [15, 15 + 51, 15 + 51 + 30, 15 + 51 + 30 + 47, 15 + 51 + 30 + 47 + 47, 51 + 30 + 47 + 47 + 26, 51 + 30 + 47 + 47 + 26+ 32],
+        fill: false,
+        borderColor: 'rgb(76, 0, 153)',
+        tension: 0.1
+    }
+    ];
+
+    const data = {
+        labels: labelsMine,
+        datasets: raceDataArray
+    };
+
+    const Cdata = {
+        labels: labelsMine,
+        datasets: championshipDataArray
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+    };
+
+    const Cconfig = {
+        type: 'line',
+        data: Cdata,
+    };
+
+    const myChart = new Chart(racesCtx, config);
+    const chChart = new Chart(championshipCtx, Cconfig);
+
+
+});
+
